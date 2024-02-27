0e7a85b982fc54e999d0f487c0a99abaa441da47
Cheems Commander <138122743+CheemsCommander@users.noreply.github.com>
2024-02-26 21:51:46 -0500

    Update block_validator.go

--- core/block_validator.go
+++ core/block_validator.go
@@ -61,7 +61,7 @@ func (v *BlockValidator) ValidateBody(block *types.Block) error {
 	//}
 	//if hash := types.CalcUncleHash(block.Uncles()); hash != header.UncleHash {
 	//	return fmt.Errorf("uncle root hash mismatch: have %x, want %x", hash, header.UncleHash)
-	/}
+	//}
 	if hash := types.DeriveSha(block.Transactions(), trie.NewStackTrie(nil)); hash != header.TxHash {
 		return fmt.Errorf("transaction root hash mismatch: have %x, want %x", hash, header.TxHash)
 	}
